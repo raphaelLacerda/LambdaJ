73a0e6a92674a995e4c904a3ef5b76250faa5699
raphaelLacerda <raphael.kaiak@gmail.com>
2012-05-11 00:25:36 -0300

    versao final

--- src/br/com/caelum/aplicacao/RunLambdaJ.java
+++ src/br/com/caelum/aplicacao/RunLambdaJ.java
@@ -1,30 +1,22 @@
 package br.com.caelum.aplicacao;
 
-import static ch.lambdaj.Lambda.avgFrom;
-import static ch.lambdaj.Lambda.by;
+import static ch.lambdaj.Lambda.convert;
 import static ch.lambdaj.Lambda.extract;
 import static ch.lambdaj.Lambda.forEach;
-import static ch.lambdaj.Lambda.group;
 import static ch.lambdaj.Lambda.having;
-import static ch.lambdaj.Lambda.index;
 import static ch.lambdaj.Lambda.on;
-import static ch.lambdaj.Lambda.project;
 import static ch.lambdaj.Lambda.select;
-import static ch.lambdaj.Lambda.selectMin;
 import static ch.lambdaj.Lambda.sort;
 import static ch.lambdaj.Lambda.sumFrom;
 import static ch.lambdaj.collection.LambdaCollections.with;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 import br.com.caelum.dominio.Palestra;
 import br.com.caelum.dominio.Palestrante;
-import br.com.caelum.infra.PalestraDTO;
+import ch.lambdaj.function.convert.Converter;
 import ch.lambdaj.function.matcher.HasArgumentWithValue;
-import ch.lambdaj.group.Group;
 
 public class RunLambdaJ {
 
@@ -34,61 +26,31 @@ public class RunLambdaJ {
 
 		List<Palestra> palestras = new ArrayList<Palestra>();
 
-		for (int i = 1; i < 100; i++) {
+		popular(palestrantes, palestras);
 
-			Palestrante palestrante = new Palestrante(i, "Aleatorio " + i, new Random().nextInt(100), new Random().nextInt(100));
-			palestrantes.add(palestrante);
+		List<String> nomesDosPalestrantes = extract(palestrantes, on(Palestrante.class).getName());
+		List<String> nomesMaiusculos = convert(nomesDosPalestrantes, new Converter<String, String>() {
 
-			palestras.add(new Palestra("palestra" + i, palestrante, new Random().nextInt(100)));
-		}
+			@Override
+			public String convert(String nomePalestrante) {
+
+				return nomePalestrante.toUpperCase();
+			}
+		});
+
+		System.out.println(nomesMaiusculos);
 
 		int totalDePosts = sumFrom(palestrantes).getPostsNoGuj();
 		System.out.println("total de posts: " + totalDePosts);
+//		
 
-		int mediaDePosts = avgFrom(palestrantes).getPostsNoGuj();
-		System.out.println("media de posts: " + mediaDePosts);
+		forEach(participanteComMaisDe50Posts(palestrantes)).favoritar();
 
-		List<Palestrante> palestrantesComPostsMaioresQue50 = select(palestrantes, comPostsMaiorQue(50));
-		
-		
-		
-		
-		forEach(palestrantesComPostsMaioresQue50).favoritar();
-		for (Palestrante palestrante : palestrantesComPostsMaioresQue50) {
-			System.out.println(palestrante);
-		}
-
-		List<String> nomesDosPalestrantes = extract(palestrantes, on(Palestrante.class).getName());
-
-		for (String nome : nomesDosPalestrantes) {
-			System.out.println(nome);
-		}
-
-		Map<Integer, Palestrante> mapaDePalestrantes = index(palestrantes, on(Palestrante.class).getId());
-
-		System.out.println(mapaDePalestrantes.size());
+		System.out.println(palestrantes);
 
 		List<Palestrante> ordenadosPorPost = sort(palestrantes, on(Palestrante.class).getPostsNoGuj());
-		List<Palestrante> ordenadosPorNome = sort(palestrantes, on(Palestrante.class).getName());
 
 		System.out.println(ordenadosPorPost);
-		System.out.println(ordenadosPorNome);
-
-		Group<Palestrante> agrupadosPorPosts = group(palestrantes, by(on(Palestrante.class).getPostsNoGuj()));
-
-		for (Palestrante palestrante : agrupadosPorPosts.findGroup("20").findAll()) {
-			System.out.println(palestrante);
-		}
-
-		//DTO
-		List<PalestraDTO> palestrantesParaTransferencia = project(palestrantes, PalestraDTO.class, on(Palestrante.class).getId(),
-				on(Palestrante.class).getName());
-
-		System.out.println(palestrantesParaTransferencia);
-
-		//Selecionar o menor post
-		Palestrante palestrantesComMenosPost = selectMin(palestrantes, on(Palestrante.class).getPostsNoGuj());
-		System.out.println(palestrantesComMenosPost);
 
 		//combinacao
 		//das palestras
@@ -96,19 +58,77 @@ public class RunLambdaJ {
 		//entao eu quero somente uma lista de nome desses palestrantes
 		//ordenadas por tempo no guj
 
-		List<Palestrante> nomeDosPalestrantesQuePostaramMaisQue80 = with(palestras).retain(comQuantidadeDeVotosMaiorQue(50)).extract(palestrante())
+		List<Palestrante> nomeDosPalestrantesCujasPalestrasTiveramMaisQue90Votos = with(palestras).retain(comQuantidadeDeVotosMaiorQue(90)).extract(palestrante())
 				.sort(porTempoNoGuj());
 
-		System.out.println(nomeDosPalestrantesQuePostaramMaisQue80);
+		System.out.println(nomeDosPalestrantesCujasPalestrasTiveramMaisQue90Votos);
+//
+//		List<String> nomes = with(palestrantes).retain(having(on(Palestrante.class).getPostsNoGuj(), greaterThanOrEqualTo(30)))
+//				.extract(on(Palestrante.class).getName()).sort(on(String.class));
+//
+//		System.out.println(nomes);
 
-		List<String> nomes = with(palestrantes).retain(having(on(Palestrante.class).getPostsNoGuj(), greaterThanOrEqualTo(30)))
-				.extract(on(Palestrante.class).getName()).sort(on(String.class));
-		
-		
-		System.out.println(nomes);
-		
-				
-		
+//
+//		int totalDePosts = sumFrom(palestrantes).getPostsNoGuj();
+//		System.out.println("total de posts: " + totalDePosts);
+//
+//		int mediaDePosts = avgFrom(palestrantes).getPostsNoGuj();
+//		System.out.println("media de posts: " + mediaDePosts);
+//
+//		for (Palestrante palestrante : palestrantesComPostsMaioresQue50) {
+//			System.out.println(palestrante);
+//		}
+//
+//		List<String> nomesDosPalestrantes = extract(palestrantes, on(Palestrante.class).getName());
+//
+//
+//		for (String nome : nomesDosPalestrantes) {
+//			System.out.println(nome);
+//		}
+//
+//		Map<Integer, Palestrante> mapaDePalestrantes = index(palestrantes, on(Palestrante.class).getId());
+//
+//		System.out.println(mapaDePalestrantes.size());
+//
+//		List<Palestrante> ordenadosPorPost = sort(palestrantes, on(Palestrante.class).getPostsNoGuj());
+//		List<Palestrante> ordenadosPorNome = sort(palestrantes, on(Palestrante.class).getName());
+//
+//		System.out.println(ordenadosPorPost);
+//		System.out.println(ordenadosPorNome);
+//
+//		Group<Palestrante> agrupadosPorPosts = group(palestrantes, by(on(Palestrante.class).getPostsNoGuj()));
+//
+//		for (Palestrante palestrante : agrupadosPorPosts.findGroup("20").findAll()) {
+//			System.out.println(palestrante);
+//		}
+//
+//		//DTO
+//		List<PalestraDTO> palestrantesParaTransferencia = project(palestrantes, PalestraDTO.class, on(Palestrante.class).getId(),
+//				on(Palestrante.class).getName());
+//
+//		System.out.println(palestrantesParaTransferencia);
+//
+//		//Selecionar o menor post
+//		Palestrante palestrantesComMenosPost = selectMin(palestrantes, on(Palestrante.class).getPostsNoGuj());
+//		System.out.println(palestrantesComMenosPost);
+//
+
+	}
+
+	private static List<Palestrante> participanteComMaisDe50Posts(List<Palestrante> palestrantes) {
+
+		return select(palestrantes, comPostsMaiorQue(50));
+	}
+
+	private static void popular(List<Palestrante> palestrantes, List<Palestra> palestras) {
+
+		for (int i = 1; i < 100; i++) {
+
+			Palestrante palestrante = new Palestrante(i, "Aleatorio " + i, new Random().nextInt(100), new Random().nextInt(100));
+			palestrantes.add(palestrante);
+
+			palestras.add(new Palestra("palestra" + i, palestrante, new Random().nextInt(100)));
+		}
 	}
 
 	private static int porTempoNoGuj() {
@@ -130,8 +150,5 @@ public class RunLambdaJ {
 
 		return having(on(Palestrante.class).getPostsNoGuj(), greaterThanOrEqualTo(valor));
 	}
-	
-	
+
 }
-
-
